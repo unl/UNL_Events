9f97d3c012f6bc124a92432db17517c18680a7b7
Tyler Lemburg <trlemburg@gmail.com>
2015-05-14 14:39:15 -0500

    Updated /manager/ URL to redirect

--- src/UNL/UCBCN/Manager/CalendarList.php
+++ src/UNL/UCBCN/Manager/CalendarList.php
@@ -4,6 +4,17 @@ namespace UNL\UCBCN\Manager;
 use UNL\UCBCN\User;
 
 class CalendarList {
+
+	public function __construct($options = array()) 
+    {
+        $calendars = $this->getCalendars();
+        if (count($calendars) > 0) {
+        	$calendars->seek(0);
+        	$calendar = $calendars->current();
+        	Controller::redirect($calendar->getManageURL());
+        }
+    }
+
     public function getCalendars() {
         $user = Auth::getCurrentUser();
 
