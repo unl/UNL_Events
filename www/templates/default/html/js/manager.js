b291b01aac018db0f895a19a4ea6d06320340d00
Michael Fairchild <mfairchild365@gmail.com>
2015-05-22 15:58:32 -0500

    Wait for DOM to be ready

--- www/templates/default/html/js/manager.js
+++ www/templates/default/html/js/manager.js
@@ -1,9 +1,7 @@
-require(['jquery', frontend_url + 'templates/default/html/js/vendor/select2/js/select2.min.js'], function($) {
-	$(".use-select2").select2();
-})
-
-require(['jquery', 'wdn', 'modernizr'], function($, WDN, Modernizr) {
+require(['jquery', 'wdn', 'modernizr', frontend_url + 'templates/default/html/js/vendor/select2/js/select2.min.js'], function($, WDN, Modernizr) {
 	$(document).ready(function() {
+		$(".use-select2").select2();
+		
 		$('.pending-event-tools').change(function () {
 			if ($(this).val() == 'recommend') {
 				// redirect to recommend URL
