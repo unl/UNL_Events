340632cad1854657fce67249cc0f349d172ab2a7
Tyler Lemburg <trlemburg@gmail.com>
2015-06-29 15:28:12 -0500

    added contsants for notice levels

--- src/UNL/UCBCN/Manager/PostHandler.php
+++ src/UNL/UCBCN/Manager/PostHandler.php
@@ -3,6 +3,10 @@ namespace UNL\UCBCN\Manager;
 
 class PostHandler
 {
+    const NOTICE_LEVEL_SUCCESS = 'success';
+    const NOTICE_LEVEL_INFO = 'info';
+    const NOTICE_LEVEL_ERROR = 'failure';
+
     public function handlePost(array $get, array $post, array $files)
     {
     	return Controller::$url;
