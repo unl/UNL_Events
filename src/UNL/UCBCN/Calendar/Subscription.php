dea4afe9086e68a96e9b05170f922fdc13945e0e
Tyler Lemburg <trlemburg@gmail.com>
2015-07-21 11:36:21 -0500

    Redrop column

--- src/UNL/UCBCN/Calendar/Subscription.php
+++ src/UNL/UCBCN/Calendar/Subscription.php
@@ -44,7 +44,6 @@ class Subscription extends Record
     public $uidcreated;                      // string(100)
     public $datelastupdated;                 // datetime(19)  binary
     public $uidlastupdated;                  // string(100)
-    public $searchcriteria;
 
     public static function getTable()
     {
