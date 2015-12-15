ec368ca59498ba7bec0b0fceeaae98d1efa99a94
Michael Fairchild <mfairchild365@gmail.com>
2015-12-15 16:11:28 -0600

    Use the 4.1 framework

---
+++ www/templates/default/html/localfooter.tpl.php
@@ -0,0 +1,20 @@
+<div class="wdn-grid-set wdn-footer-links-local">
+    <div class="bp960-wdn-col-two-thirds">
+        <div class="wdn-footer-module">
+            <span role="heading" class="wdn-footer-heading">About UNL Events</span>
+            <p>
+                This application is developed and maintained by <a href="http://iim.unl.edu/">Internet and Interactive Media</a> (IIM), which is a partnership with University Communications and Information Technology Services.
+            </p>
+        </div>
+    </div>
+    <div class="bp960-wdn-col-one-third">
+        <div class="wdn-footer-module">
+            <span role="heading" class="wdn-footer-heading">Related Links</span>
+            <ul class="wdn-related-links-v1">
+                <li><a href="http://iim.unl.edu/">Internet and Interactive Media</a></li>
+                <li><a href="http://ucomm.unl.edu/">University Communications</a></li>
+                <li><a href="http://its.unl.edu/">Information Technology Services</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
