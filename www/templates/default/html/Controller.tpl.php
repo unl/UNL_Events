e22be503145adfd1a3e6f6256b2988c3d60a99bf
Tyler Lemburg <trlemburg@gmail.com>
2016-01-22 13:21:22 -0600

    Change wording

--- www/templates/default/html/Controller.tpl.php
+++ www/templates/default/html/Controller.tpl.php
@@ -59,7 +59,7 @@ if ($context->getCalendar()) {
                         <div class="wdn-col-full">
                             <div class="events-nav">
                                 <div class="submit-search">
-                                    <a id="frontend_login" class="eventicon-plus-circled" href="' . UNL\UCBCN\Frontend\Controller::$manager_url . $context->getCalendar()->shortname . '">Submit an Event</a>
+                                    <a id="frontend_login" class="eventicon-plus-circled" href="' . UNL\UCBCN\Frontend\Controller::$manager_url . $context->getCalendar()->shortname . '">Manage Events</a>
                                     <form id="event_search" method="get" action="' . $frontend->getCalendarURL() . 'search/" role="search">
                                         <label for="searchinput">Search Events</label>
                                         <div class="wdn-input-group">
