646b4c813a5ba96b9e5d6138bb148c6ee4896046
Michael Fairchild <mfairchild365@gmail.com>
2017-10-17 15:19:27 -0500

    Improve the accessibility of section headings
    Links should not be a child of the h1 element, and this also fixes the tab order of those links.

--- www/templates/default/hcalendar/Upcoming.tpl.php
+++ www/templates/default/hcalendar/Upcoming.tpl.php
@@ -1,6 +1,10 @@
-<h1 class="upcoming-heading">
-    Upcoming Events
-    <a href="<?php echo $frontend->getCalendarURL(); ?>upcoming/.ics"><span class="wdn-icon-calendar" aria-hidden="true"></span><span class="wdn-text-hidden">ics format for upcoming events</span></a>
-</h1>
+<div class="section-heading">
+    <h1 class="upcoming-heading">
+        Upcoming Events
+    </h1>
+    <div class="links">
+        <a href="<?php echo $frontend->getCalendarURL(); ?>upcoming/.ics"><span class="wdn-icon-calendar" aria-hidden="true"></span><span class="wdn-text-hidden">ics format for upcoming events</span></a>
+    </div>
+</div>
 
 <?php echo $savvy->render($context, 'EventListing.tpl.php');?>
