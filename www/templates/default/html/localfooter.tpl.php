2e8c61817cf50f342db8fab1c55c4c9abd5fbcd2
Michael Fairchild <mfairchild365@gmail.com>
2018-08-08 13:20:09 -0500

    implement role="heading" correctly

--- www/templates/default/html/localfooter.tpl.php
+++ www/templates/default/html/localfooter.tpl.php
@@ -1,13 +1,13 @@
 <div class="wdn-grid-set wdn-footer-links-local">
     <div class="bp960-wdn-col-two-thirds">
         <div class="wdn-footer-module">
-            <span role="heading" class="wdn-footer-heading">About UNL Events</span>
+            <span role="heading" aria-level="2" class="wdn-footer-heading">About UNL Events</span>
             <p>This application is a product of the <a href="https://iim.unl.edu/">Internet and Interactive Media group at Nebraska</a>. IIM is a partnership of <a href="https://ucomm.unl.edu/">University Communication</a> and <a href="https://its.unl.edu/">Information Technology Services</a>.</p>
         </div>
     </div>
     <div class="bp960-wdn-col-one-third">
         <div class="wdn-footer-module">
-            <span role="heading" class="wdn-footer-heading">Related Links</span>
+            <span role="heading" aria-level="2" class="wdn-footer-heading">Related Links</span>
             <ul class="wdn-related-links-v1">
                 <li><a href="http://wdn.unl.edu/documentation/unl-events">UNL Events Documentation</a></li>
                 <li><a href="http://wdn.unl.edu/">Web Developer Network</a></li>
