507acd1c55b8be90e9ecb9beff069dd79e7a2f2e
Jeffrey Sturek <jsturek8@unl.edu>
2018-12-17 11:35:36 -0600

    WIP design tweaks

--- www/templates/default/html/EventListing.tpl.php
+++ www/templates/default/html/EventListing.tpl.php
@@ -19,16 +19,22 @@
         ?>
         <div class="<?php echo implode(' ', $row_classes) ?>">
           <?php echo $savvy->render($eventinstance, 'EventInstance/Summary.tpl.php') ?>
-          <div class="dcf-grid dcf-col-gap-3">
-            <div class="dcf-col-100% dcf-col-25%-start@sm dcf-pb-3">
-                <?php echo $savvy->render($eventinstance, 'EventInstance/Thumbnail.tpl.php') ?>
+          <?php if ($eventinstance->getImageURL()) { ?>
+            <div class="dcf-grid dcf-col-gap-4">
+              <div class="dcf-col-100% dcf-col-75%-start@sm dcf-pb-3">
+                  <?php echo $savvy->render($eventinstance, 'EventInstance/Date.tpl.php') ?>
+                  <?php echo $savvy->render($eventinstance, 'EventInstance/Location.tpl.php') ?>
+                  <?php echo $savvy->render($eventinstance, 'EventInstance/Description.tpl.php') ?>
+              </div>
+              <div class="dcf-col-100% dcf-col-25%-end@sm">
+                  <?php echo $savvy->render($eventinstance, 'EventInstance/Thumbnail.tpl.php') ?>
+              </div>
             </div>
-            <div class="dcf-col-100% dcf-col-75%-end@sm">
-                <?php echo $savvy->render($eventinstance, 'EventInstance/Date.tpl.php') ?>
-                <?php echo $savvy->render($eventinstance, 'EventInstance/Location.tpl.php') ?>
-                <?php echo $savvy->render($eventinstance, 'EventInstance/Description.tpl.php') ?>
-            </div>
-          </div>
+          <?php } else { ?>
+              <?php echo $savvy->render($eventinstance, 'EventInstance/Date.tpl.php') ?>
+              <?php echo $savvy->render($eventinstance, 'EventInstance/Location.tpl.php') ?>
+              <?php echo $savvy->render($eventinstance, 'EventInstance/Description.tpl.php') ?>
+          <?php } // end else ?>
         </div>
         <?php
     }
