ec2f6e3579598394e52f5999927948588a5afeec
Jeffrey Sturek <jsturek8@unl.edu>
2019-04-09 16:41:42 -0500

    Update calendar home link to current calendar instead of main calendar

--- www/templates/default/html/Controller.tpl.php
+++ www/templates/default/html/Controller.tpl.php
@@ -22,7 +22,7 @@ if (!$context->getCalendar()) {
 }
 
 $page->doctitle = '<title>' . $title . '</title>';
-$page->titlegraphic = '<a class="dcf-txt-h5" href="/">' . $site_title . '</a>';
+$page->titlegraphic = '<a class="dcf-txt-h5" href="' . $context->getCalendarURL() . '">' . $site_title . '</a>';
 $page->setParam('class', 'hide-wdn_navigation_wrapper');
 $page->affiliation = '';
 
