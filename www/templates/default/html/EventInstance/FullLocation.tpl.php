5eb9cd4a528b95cca5bb3f3c1d641d036d5d7984
Ryan Dee <ryan@skoolbus39.com>
2020-01-30 14:13:43 -0600

    Replace event details font icons with SVGs
    Update icons for date, time and location.

--- www/templates/default/html/EventInstance/FullLocation.tpl.php
+++ www/templates/default/html/EventInstance/FullLocation.tpl.php
@@ -8,7 +8,10 @@ $directions = !empty($context->eventdatetime->directions) ? $context->eventdatet
 ?>
 <?php if ($locationName || $room || $directions || isset($location->streetaddress1)): ?>
 <div class="location">
-    <span class="eventicon-location" aria-hidden="true"></span>
+    <svg class="dcf-mr-1 dcf-h-4 dcf-w-4 dcf-fill-current" aria-hidden="true" focusable="false" height="24" width="24" viewBox="0 0 24 24">
+        <path d="M12 0C7.589 0 4 3.589 4 8c0 4.245 7.273 15.307 7.583 15.775a.497.497 0 00.834 0C12.727 23.307 20 12.245 20 8c0-4.411-3.589-8-8-8zm0 22.58C10.434 20.132 5 11.396 5 8c0-3.86 3.14-7 7-7s7 3.14 7 7c0 3.395-5.434 12.132-7 14.58z"></path>
+        <path d="M12 4.5c-1.93 0-3.5 1.57-3.5 3.5s1.57 3.5 3.5 3.5 3.5-1.57 3.5-3.5-1.57-3.5-3.5-3.5zm0 6c-1.378 0-2.5-1.122-2.5-2.5s1.122-2.5 2.5-2.5 2.5 1.122 2.5 2.5-1.122 2.5-2.5 2.5z"></path>
+    </svg>
 <?php if (isset($location->mapurl)): ?>
     <a class="mapurl" href="<?php echo $location->mapurl ?>"><?php echo $location->name; ?></a>
 <?php else: ?>
