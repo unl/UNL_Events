bb05da52d26f318224abc6f950dd87cf3110ccb0
Ryan Dee <ryan@skoolbus39.com>
2020-01-30 15:32:46 -0600

    Replace breadcrumb bar search font icon with SVG

--- www/templates/default/html/manager/BreadcrumbBar.tpl.php
+++ www/templates/default/html/manager/BreadcrumbBar.tpl.php
@@ -16,7 +16,12 @@
 
 <?php if (isset($context->search)): ?>
 <div id="toggle-search">
-	<button id="show-search"><span class="wdn-icon-search" aria-hidden="true"></span><span class="dcf-sr-only">search</span></button>
+	<button id="show-search">
+      <svg class="dcf-h-5 dcf-w-5 dcf-fill-current" aria-hidden="true" focusable="false" height="16" width="16" viewBox="0 0 48 48">
+          <path d="M18 36a17.9 17.9 0 0 0 11.27-4l15.31 15.41a2 2 0 0 0 2.84-2.82L32.08 29.18A18 18 0 1 0 18 36zm0-32A14 14 0 1 1 4 18 14 14 0 0 1 18 4z"></path>
+      </svg>
+      <span class="dcf-sr-only">Search</span>
+	</button>
 </div>
 <?php endif; ?>
 </nav>
