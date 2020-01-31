84f34d0a34af6cbd915d39d07c51f21391ab92f8
Ryan Dee <ryan@skoolbus39.com>
2020-01-31 11:05:04 -0600

    Update version date for cache busting

--- src/UNL/UCBCN/Frontend/Controller.php
+++ src/UNL/UCBCN/Frontend/Controller.php
@@ -79,7 +79,7 @@ class Controller
         'format' => 'html',
     );
 
-    public static $version = '2020-01-15';
+    public static $version = '2020-01-31';
 
     /**
      * Constructor for the frontend.
