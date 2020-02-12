acfe4e24c0e58963445eadfcfd73822869fce3ca
Ryan Dee <ryan@skoolbus39.com>
2020-02-12 12:18:47 -0600

    Update breadcrumbs/search bar
    - Replace custom styles with utility classes
    - Remove WDN classes

--- www/templates/default/html/manager/BreadcrumbBar.tpl.php
+++ www/templates/default/html/manager/BreadcrumbBar.tpl.php
@@ -1,8 +1,7 @@
-<nav id="breadcrumbs" <?php if (isset($context->search)) echo 'class="search"' ?>>
+<nav class="dcf-relative dcf-overflow-hidden dcf-mb-6 dcf-pr-8 dcf-txt-md" id="breadcrumbs" <?php if (isset($context->search)) echo 'class="search"' ?>>
     <!-- WDN: see glossary item 'breadcrumbs' -->
-    <h3 class="wdn_list_descriptor dcf-sr-only">Breadcrumbs</h3>
-    
-    <ul>
+    <h3 class="dcf-sr-only">Breadcrumbs</h3>
+    <ul class="dcf-mb-0">
         <?php foreach ($context->crumbs as $text => $url): ?>
         <li>
             <?php if ($url != NULL): ?>
@@ -13,10 +12,9 @@
         </li>
         <?php endforeach; ?>
     </ul>
-    
     <?php if (isset($context->search)): ?>
-    <div id="toggle-search">
-        <button id="show-search">
+    <div class="dcf-absolute dcf-pin-top dcf-pin-right" id="toggle-search">
+        <button class="dcf-btn dcf-btn-primary" id="show-search">
             <svg class="dcf-h-5 dcf-w-5 dcf-fill-current" aria-hidden="true" focusable="false" height="16" width="16" viewBox="0 0 48 48">
                 <path d="M18 36a17.9 17.9 0 0 0 11.27-4l15.31 15.41a2 2 0 0 0 2.84-2.82L32.08 29.18A18 18 0 1 0 18 36zm0-32A14 14 0 1 1 4 18 14 14 0 0 1 18 4z"></path>
             </svg>
