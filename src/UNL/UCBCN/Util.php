0f8c4e60dc6d79ff3f866df27ecbc9ff4d619bae
Jeff Sturek <jsturek8@unl.edu>
2020-02-18 13:45:11 -0600

    Tweak phone format

--- src/UNL/UCBCN/Util.php
+++ src/UNL/UCBCN/Util.php
@@ -9,9 +9,16 @@ class Util
     }
 
     public static function formatPhoneNumber($phoneNumber) {
+
+        // Do not format phone numbers containing letters
+        if (preg_match('/[a-z]/i', $phoneNumber)) {
+            return $phoneNumber;
+        }
+
         $phoneNumber = preg_replace('/[^0-9]/','', $phoneNumber);
 
-        if(strlen($phoneNumber) > 10) {
+        // Format > 10 digit phone number
+        if (strlen($phoneNumber) > 10) {
             $countryCode = substr($phoneNumber, 0, strlen($phoneNumber)-10);
             $areaCode = substr($phoneNumber, -10, 3);
             $nextThree = substr($phoneNumber, -7, 3);
@@ -19,19 +26,32 @@ class Util
 
             $phoneNumber = '+'.$countryCode.' ('.$areaCode.') '.$nextThree.'-'.$lastFour;
         }
-        else if(strlen($phoneNumber) == 10) {
+
+        // Format 10 digit phone number
+        else if (strlen($phoneNumber) == 10) {
             $areaCode = substr($phoneNumber, 0, 3);
             $nextThree = substr($phoneNumber, 3, 3);
             $lastFour = substr($phoneNumber, 6, 4);
 
             $phoneNumber = '('.$areaCode.') '.$nextThree.'-'.$lastFour;
         }
-        else if(strlen($phoneNumber) == 7) {
+
+        // Format 7 digit phone number
+        else if (strlen($phoneNumber) == 7) {
             $nextThree = substr($phoneNumber, 0, 3);
             $lastFour = substr($phoneNumber, 3, 4);
 
             $phoneNumber = $nextThree.'-'.$lastFour;
         }
+
+        // Format shorthand UNL phone number in full format
+        else if (strlen($phoneNumber) == 5 && substr($phoneNumber, 0, 1) == 2) {
+            $lastFour = substr($phoneNumber, 1, 4);
+            $phoneNumber = '402-472-'.$lastFour;
+        }
+
+        // note: if no strlen conditionals are met for numeric phone number,
+        // no formatting is done.
 
         return $phoneNumber;
     }
