795ad718485a24c4ceccaa29549002f9d308bfcf
Jeff Sturek <jsturek8@unl.edu>
2020-12-18 11:20:45 -0600

    Fix Typo

--- www/templates/default/html/EventInstance/FullLocation.tpl.php
+++ www/templates/default/html/EventInstance/FullLocation.tpl.php
@@ -12,9 +12,9 @@ $directions = !empty($context->eventdatetime->directions) ? $context->eventdatet
         <path d="M12 0C7.589 0 4 3.589 4 8c0 4.245 7.273 15.307 7.583 15.775a.497.497 0 00.834 0C12.727 23.307 20 12.245 20 8c0-4.411-3.589-8-8-8zm0 22.58C10.434 20.132 5 11.396 5 8c0-3.86 3.14-7 7-7s7 3.14 7 7c0 3.395-5.434 12.132-7 14.58z"></path>
         <path d="M12 4.5c-1.93 0-3.5 1.57-3.5 3.5s1.57 3.5 3.5 3.5 3.5-1.57 3.5-3.5-1.57-3.5-3.5-3.5zm0 6c-1.378 0-2.5-1.122-2.5-2.5s1.122-2.5 2.5-2.5 2.5 1.122 2.5 2.5-1.122 2.5-2.5 2.5z"></path>
     </svg>
-<?php if (isset($location->mapurl) && filter_var($context->mapurl, FILTER_VALIDATE_URL)): ?>
+<?php if (isset($location->mapurl) && filter_var($location->mapurl, FILTER_VALIDATE_URL)): ?>
     <a class="mapurl" href="<?php echo $location->mapurl ?>"><?php echo $location->name; ?></a>
-<?php elseif (isset($location->webpageurl) && filter_var($context->webpageurl, FILTER_VALIDATE_URL)): ?>
+<?php elseif (isset($location->webpageurl) && filter_var($location->webpageurl, FILTER_VALIDATE_URL)): ?>
     <a class="webpageurl" href="<?php echo $location->webpageurl ?>"><?php echo $location->name; ?></a>
 <?php else: ?>
     <?php echo $location->name; ?>
