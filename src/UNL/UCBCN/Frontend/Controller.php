8afde0ee81e8e609165d729ee1b1748a0664904b
Jeff Sturek <jsturek8@unl.edu>
2021-01-05 13:55:02 -0600

    Style manager selects

--- src/UNL/UCBCN/Frontend/Controller.php
+++ src/UNL/UCBCN/Frontend/Controller.php
@@ -79,7 +79,7 @@ class Controller
         'format' => 'html',
     );
 
-    public static $version = '2020-02-18';
+    public static $version = '20210105';
 
     /**
      * Constructor for the frontend.
