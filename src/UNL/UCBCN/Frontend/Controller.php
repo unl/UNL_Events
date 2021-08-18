94562f3eb84fd4b8c283e8e6887fe85306390f65
Jeff Sturek <jsturek@gmail.com>
2021-08-18 10:22:39 -0500

    Update cache bust string

--- src/UNL/UCBCN/Frontend/Controller.php
+++ src/UNL/UCBCN/Frontend/Controller.php
@@ -79,7 +79,7 @@ class Controller
         'format' => 'html',
     );
 
-    public static $version = '2021818';
+    public static $version = '20210825';
 
     /**
      * Constructor for the frontend.
