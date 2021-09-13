f00d0236726cc300d20e00c3998d6d7706f8482a
Ryan Dee <ryan@skoolbus39.com>
2021-09-13 12:07:37 -0500

    Update Users markup

--- www/templates/default/html/manager/Users.tpl.php
+++ www/templates/default/html/manager/Users.tpl.php
@@ -9,38 +9,35 @@
 	//$calendar = Calendar::getByShortname($this->options['calendar_shortname']);
 ?>
 
-<h2 class="wdn-brand">Users on this Calendar</h2>
+<h1>Users on this Calendar</h1>
 <div>
-	<table>
-		<thead>
-			<tr>
-				<th>User</th>
-				<th>Actions</th>
-			</tr>
-		</thead>
-		<tbody>
-	    <?php foreach($context->getUsers() as $user): ?>
-	    <tr>
-			<td>
-	        	<?php echo $user->uid; ?>
-	    	</td>
-	    	<td class="small-center table-actions">
-		        <a class="dcf-btn dcf-btn-primary" href="<?php echo $user->getEditPermissionsURL($context->calendar) ?>">Edit Permissions</a>
-		        <span class="small-hidden">|</span><br class="dcf-d-none small-block" /><br class="dcf-d-none small-block" />
-		        <form method="post" action="<?php echo $user->getDeletePermissionsURL($context->calendar) ?>" class="delete-form">
-                  <input type="hidden" name="<?php echo $controller->getCSRFHelper()->getTokenNameKey() ?>" value="<?php echo $controller->getCSRFHelper()->getTokenName() ?>" />
-                  <input type="hidden" name="<?php echo $controller->getCSRFHelper()->getTokenValueKey() ?>" value="<?php echo $controller->getCSRFHelper()->getTokenValue() ?>">
-	                <input type="hidden" name="user_uid" value="<?php echo $user->uid ?>" />
-	                <button class="dcf-btn" type="submit">Remove</button>
-	            </form>
-            </td>
-	    </tr>
-	    </tbody>
-	    <?php endforeach; ?>
-	</table>
+    <table>
+        <thead>
+            <tr>
+                <th>User</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($context->getUsers() as $user): ?>
+            <tr>
+                <td>
+                    <?php echo $user->uid; ?>
+                </td>
+                <td class="small-center table-actions">
+                    <a class="dcf-btn dcf-btn-primary" href="<?php echo $user->getEditPermissionsURL($context->calendar) ?>">Edit Permissions</a>
+                    <span class="small-hidden">|</span><br class="dcf-d-none small-block" /><br class="dcf-d-none small-block" />
+                    <form method="post" action="<?php echo $user->getDeletePermissionsURL($context->calendar) ?>" class="delete-form">
+                        <input type="hidden" name="<?php echo $controller->getCSRFHelper()->getTokenNameKey() ?>" value="<?php echo $controller->getCSRFHelper()->getTokenName() ?>" />
+                        <input type="hidden" name="<?php echo $controller->getCSRFHelper()->getTokenValueKey() ?>" value="<?php echo $controller->getCSRFHelper()->getTokenValue() ?>">
+                        <input type="hidden" name="user_uid" value="<?php echo $user->uid ?>" />
+                        <button class="dcf-btn dcf-btn-primary" type="submit">Remove</button>
+                    </form>
+                </td>
+            </tr>
+        </tbody>
+       <?php endforeach; ?>
+    </table>
 </div>
 <br>
-
-<a href="<?php echo $base_manager_url . $context->calendar->shortname ?>/users/new/" 
-	class="dcf-btn dcf-btn-primary">Add User
-</a><br>
+<a class="dcf-btn dcf-btn-primary" href="<?php echo $base_manager_url . $context->calendar->shortname ?>/users/new/">Add User</a>
