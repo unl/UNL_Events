9a082272cb33dfb24597d37c180ec1028f1c8fcc
Ryan Dee <ryan@skoolbus39.com>
2021-09-13 14:32:35 -0500

    Update Recommend markup

--- www/templates/default/html/manager/Recommend.tpl.php
+++ www/templates/default/html/manager/Recommend.tpl.php
@@ -7,23 +7,15 @@
     );
     echo $savvy->render($crumbs, 'BreadcrumbBar.tpl.php');
 ?>
-<h2 class="wdn-brand">
-<?php echo 'Recommend ' . $context->event->title; ?>
-</h2>
-<form action="" method="POST">
+<h1><?php echo 'Recommend ' . $context->event->title; ?></h1>
+<form class="dcf-form" action="" method="POST">
     <input type="hidden" name="<?php echo $controller->getCSRFHelper()->getTokenNameKey() ?>" value="<?php echo $controller->getCSRFHelper()->getTokenName() ?>" />
     <input type="hidden" name="<?php echo $controller->getCSRFHelper()->getTokenValueKey() ?>" value="<?php echo $controller->getCSRFHelper()->getTokenValue() ?>">
     <table class="recommend-list">
         <thead>
-            <th>
-                &nbsp;
-            </th>
-            <th class="center">
-                Pending
-            </th>
-            <th class="center">
-                Approved
-            </th>
+            <th>&nbsp;</th>
+            <th class="center">Pending</th>
+            <th class="center">Approved</th>
         </thead>
         <tbody>
         <?php foreach($context->getRecommendableCalendars() as $id => $arr): ?>
@@ -36,7 +28,7 @@
                 <?php if ($arr['status'] == 'pending') { ?>
                     <img src="<?php echo $base_frontend_url ?>templates/default/html/images/checkmark-16.png" alt="Event is Pending">
                 <?php } else if ($arr['can_pending']) { ?>
-                    <input class="dcf-input-control" type="radio" title="<?php echo $calendar->name ?>: Pending" name="calendar_<?php echo $calendar->id ?>" value="pending">
+                    <input class="dcf-input-control" name="calendar_<?php echo $calendar->id ?>" type="radio" value="pending" title="<?php echo $calendar->name ?>: Pending">
                 <?php } else { ?>
                     &nbsp;
                 <?php } ?>
@@ -46,7 +38,7 @@
                 <?php if ($arr['status'] == 'posted' || $arr['status'] == 'archived') { ?>
                     <img src="<?php echo $base_frontend_url ?>templates/default/html/images/checkmark-16.png" alt="Event is Upcoming">
                 <?php } else if ($arr['can_posted']) { ?>
-                    <input class="dcf-input-control" type="radio" title="<?php echo $calendar->name ?>: Upcoming" name="calendar_<?php echo $calendar->id ?>" value="posted">
+                    <input class="dcf-input-control" name="calendar_<?php echo $calendar->id ?>" type="radio" value="posted" title="<?php echo $calendar->name ?>: Upcoming">
                 <?php } else { ?>
                     &nbsp;
                 <?php } ?>
@@ -55,8 +47,5 @@
         <?php endforeach; ?>
         </tbody>
     </table>
-    <br>
-    <button class="dcf-btn dcf-btn-primary" type="submit">
-        Submit
-    </button>
+    <button class="dcf-btn dcf-btn-primary" type="submit">Submit</button>
 </form>
