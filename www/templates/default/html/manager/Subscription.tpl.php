0d584a985cf9116fd0e47880db9a3d33fa1893b0
Ryan Dee <ryan@skoolbus39.com>
2021-09-13 12:10:54 -0500

    Update Subscription markup

--- www/templates/default/html/manager/Subscription.tpl.php
+++ www/templates/default/html/manager/Subscription.tpl.php
@@ -1,11 +1,11 @@
 <?php
-	$crumbs = new stdClass;
-	$crumbs->crumbs = array(
-		"Events Manager" => "/manager",
-		$context->calendar->name => $context->calendar->getManageURL(),
-		"Subscriptions" => NULL
-	);
-	echo $savvy->render($crumbs, 'BreadcrumbBar.tpl.php');
+  $crumbs = new stdClass;
+  $crumbs->crumbs = array(
+    "Events Manager" => "/manager",
+    $context->calendar->name => $context->calendar->getManageURL(),
+    "Subscriptions" => NULL
+  );
+  echo $savvy->render($crumbs, 'BreadcrumbBar.tpl.php');
 ?>
 
 <?php if (count($context->getSubscriptions()) > 0): ?>
@@ -17,39 +17,33 @@
     <button type="submit">Submit</button>
 </form>
 <?php endforeach; ?>
-<h2 class="wdn-brand">Current Subscriptions</h2>
+<h1>Current Subscriptions</h1>
 <div>
-	<table>
-		<thead>
-			<tr>
-				<th>Title</th>
-				<th>Actions</th>
-			</tr>
-		</thead>
-		<tbody>
-	    <?php foreach($context->getSubscriptions() as $subscription): ?>
-			<tr>
-				<td>
-		        	<?php echo $subscription->name; ?>
-				</td>
-				<td class="small-center table-actions">
-			        <a class="dcf-btn dcf-btn-primary" href="<?php echo $subscription->getEditURL() ?>">Edit</a>
-			        <span class="small-hidden">|</span><br class="dcf-d-none small-block"><br class="dcf-d-none small-block">
-		            <button class="dcf-btn dcf-btn-secondary" form="delete-subscription-<?php echo $subscription->id ?>" type="submit">Delete</button>
-				</td>
-			</tr>
-	    <?php endforeach; ?>
-		</tbody>
-	</table>
+    <table>
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($context->getSubscriptions() as $subscription): ?>
+            <tr>
+                <td>
+                    <?php echo $subscription->name; ?>
+                </td>
+                <td class="small-center table-actions">
+                    <a class="dcf-btn dcf-btn-primary" href="<?php echo $subscription->getEditURL() ?>">Edit</a>
+                    <span class="small-hidden">|</span><br class="dcf-d-none small-block"><br class="dcf-d-none small-block">
+                    <button class="dcf-btn dcf-btn-secondary" form="delete-subscription-<?php echo $subscription->id ?>" type="submit">Delete</button>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 </div>
 <br>
 <?php else: ?>
-	<br>
-	<label>There are currently no subscriptions on this calendar.</label>
-	<br>
+    <p>There are currently no subscriptions on this calendar.</p>
 <?php endif; ?>
-
-<br>
-<a href="<?php echo $base_manager_url . $context->calendar->shortname ?>/subscriptions/new/" 
-	class="dcf-btn dcf-btn-primary">Add Subscription
-</a>
+<a class="dcf-btn dcf-btn-primary" href="<?php echo $base_manager_url . $context->calendar->shortname ?>/subscriptions/new/">Add Subscription</a>
