41684325c8aa9074b59914b48aa678536cdfabc8
Ryan Dee <ryan@skoolbus39.com>
2021-09-14 12:02:22 -0500

    Fix radio buttons and spacing in Recommend

--- www/templates/default/html/manager/Recommend.tpl.php
+++ www/templates/default/html/manager/Recommend.tpl.php
@@ -11,7 +11,7 @@
 <form class="dcf-form" action="" method="POST">
     <input type="hidden" name="<?php echo $controller->getCSRFHelper()->getTokenNameKey() ?>" value="<?php echo $controller->getCSRFHelper()->getTokenName() ?>" />
     <input type="hidden" name="<?php echo $controller->getCSRFHelper()->getTokenValueKey() ?>" value="<?php echo $controller->getCSRFHelper()->getTokenValue() ?>">
-    <table class="recommend-list">
+    <table class="recommend-list dcf-mb-5">
         <thead>
             <th>&nbsp;</th>
             <th class="center">Pending</th>
@@ -28,9 +28,11 @@
                 <?php if ($arr['status'] == 'pending') { ?>
                     <img src="<?php echo $base_frontend_url ?>templates/default/html/images/checkmark-16.png" alt="Event is Pending">
                 <?php } else if ($arr['can_pending']) { ?>
-                    <div class="dcf-input-checkbox">
-                        <input id="event-<?php echo $calendar->id ?>-is-pending" name="calendar_<?php echo $calendar->id ?>" type="radio" value="pending" title="<?php echo $calendar->name ?>: Pending">
-                        <label class="dcf-sr-only" for="event-<?php echo $calendar->id ?>-is-pending">Pending</label>
+                    <div class="dcf-d-flex dcf-jc-center">
+                        <div class="dcf-input-radio">
+                            <input id="event-<?php echo $calendar->id ?>-is-pending" name="calendar_<?php echo $calendar->id ?>" type="radio" value="pending" title="<?php echo $calendar->name ?>: Pending">
+                            <label for="event-<?php echo $calendar->id ?>-is-pending"><span class="dcf-sr-only">Pending</span></label>
+                        </div>
                     </div>
                 <?php } else { ?>
                     &nbsp;
@@ -41,9 +43,11 @@
                 <?php if ($arr['status'] == 'posted' || $arr['status'] == 'archived') { ?>
                     <img src="<?php echo $base_frontend_url ?>templates/default/html/images/checkmark-16.png" alt="Event is Upcoming">
                 <?php } else if ($arr['can_posted']) { ?>
-                    <div class="dcf-input-checkbox">
-                        <input id="event-<?php echo $calendar->id ?>-is-upcoming" name="calendar_<?php echo $calendar->id ?>" type="radio" value="posted" title="<?php echo $calendar->name ?>: Upcoming">
-                        <label class="dcf-sr-only" for="event-<?php echo $calendar->id ?>-is-upcoming">Upcoming</label>
+                    <div class="dcf-d-flex dcf-jc-center">
+                        <div class="dcf-input-radio">
+                            <input id="event-<?php echo $calendar->id ?>-is-upcoming" name="calendar_<?php echo $calendar->id ?>" type="radio" value="posted" title="<?php echo $calendar->name ?>: Upcoming">
+                            <label for="event-<?php echo $calendar->id ?>-is-upcoming"><span class="dcf-sr-only">Upcoming</span></label>
+                        </div>
                     </div>
                 <?php } else { ?>
                     &nbsp;
