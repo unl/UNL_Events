41db505238e2c8c35cba23b681450171db299aed
Jeff Sturek <jsturek@gmail.com>
2021-11-09 14:54:32 -0600

    Cleanup code duplication

--- src/UNL/UCBCN/Manager/AddDatetime.php
+++ src/UNL/UCBCN/Manager/AddDatetime.php
@@ -157,17 +157,6 @@ class AddDatetime extends PostHandler
         }
     }
 
-    private function calculateDate($date, $hour, $minute, $am_or_pm)
-    {
-        # defaults if NULL is passed in
-        $hour = $hour == NULL ? 12 : $hour;
-        $minute = $minute == NULL ? 0 : $minute;
-        $am_or_pm = $am_or_pm == NULL ? 'am' : $am_or_pm;
-
-        $date = strtotime($date . ' ' . $hour . ':' . $minute . ':00 ' . $am_or_pm);
-        return date('Y-m-d H:i:s', $date);
-    }
-
     private function setDatetimeData($post_data)
     {
         # set the start date and end date
@@ -232,25 +221,8 @@ class AddDatetime extends PostHandler
             throw new ValidationException('You must give your new location a <a href="#location-name">name</a>.');
         }
 
+        # Validate Recurring Event (if applicable)
         $this->validateRecurringEvent($post_data, $start_date, $end_date);
-    }
-
-	private function validateRecurringEvent($post_data, $start_date, $end_date) {
-        # check that recurring events have recurring type and correct recurs until date
-        if (array_key_exists('recurring', $post_data) && $post_data['recurring'] == 'on') {
-            if (empty($post_data['recurring_type']) || empty($post_data['recurs_until_date'])) {
-                throw new ValidationException('Recurring events require a <a href="#recurring-type">recurring type</a> and <a href="#recurs-until-date">date</a> that they recur until.');
-            }
-
-            $recurs_until = $this->calculateDate($post_data['recurs_until_date'], 11, 59, 'PM');
-            if ($start_date > $recurs_until) {
-                throw new ValidationException('The <a href="#recurs-until-date">"recurs until date"</a> must be on or after the start date.');
-            }
-
-            if (date('d', strtotime($start_date)) !== date('d', strtotime($end_date))) {
-                throw new ValidationException('A recurring event instance start and end date must be the same day. If you need multiple multi-day (ongoing) occurrences, you must define them as separate datetime instances.');
-            }
-        }
     }
 
     public function editDatetime($post_data)
--- src/UNL/UCBCN/Manager/PostHandler.php
+++ src/UNL/UCBCN/Manager/PostHandler.php
@@ -10,15 +10,45 @@ class PostHandler
 
     public function handlePost(array $get, array $post, array $files)
     {
-    	return Controller::$url;
+        return Controller::$url;
     }
 
     public function flashNotice($level, $header, $message)
     {
-    	$_SESSION['flash_notice'] = array(
-    		'level' => $level,
-    		'header' => $header,
-    		'messageHTML' => $message
-    	);
+        $_SESSION['flash_notice'] = array(
+            'level' => $level,
+            'header' => $header,
+            'messageHTML' => $message
+        );
+    }
+
+    // Shared validation for recurring event
+    protected function validateRecurringEvent($post_data, $start_date, $end_date) {
+        # check that recurring events have recurring type and correct recurs until date
+        if (array_key_exists('recurring', $post_data) && $post_data['recurring'] == 'on') {
+            if (empty($post_data['recurring_type']) || empty($post_data['recurs_until_date'])) {
+                throw new ValidationException('Recurring events require a <a href="#recurring-type">recurring type</a> and <a href="#recurs-until-date">date</a> that they recur until.');
+            }
+
+            $recurs_until = $this->calculateDate($post_data['recurs_until_date'], 11, 59, 'PM');
+            if ($start_date > $recurs_until) {
+                throw new ValidationException('The <a href="#recurs-until-date">"recurs until date"</a> must be on or after the start date.');
+            }
+
+            if (date('d', strtotime($start_date)) !== date('d', strtotime($end_date))) {
+                throw new ValidationException('A recurring event instance start and end date must be the same day. If you need multiple multi-day (ongoing) occurrences, you must define them as separate datetime instances.');
+            }
+        }
+    }
+
+    protected function calculateDate($date, $hour, $minute, $am_or_pm)
+    {
+        # defaults if NULL is passed in
+        $hour = $hour == NULL ? 12 : $hour;
+        $minute = $minute == NULL ? 0 : $minute;
+        $am_or_pm = $am_or_pm == NULL ? 'am' : $am_or_pm;
+
+        $date = strtotime($date . ' ' . $hour . ':' . $minute . ':00 ' . $am_or_pm);
+        return date('Y-m-d H:i:s', $date);
     }
 }
