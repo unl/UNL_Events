c7f6fb5736a141c65c42d4c4e7a4ae3b16cf1e08
Thomas Neumann <thomasneumann21800@gmail.com>
2023-02-21 13:43:22 -0600

    Moved badge test

--- www/templates/default/html/EventInstance/Summary.tpl.php
+++ www/templates/default/html/EventInstance/Summary.tpl.php
@@ -5,21 +5,19 @@
 ?>
 
 <?php if (isset($context->event->subtitle)): ?><header><?php endif; ?>
-    <h2>
-        <span class="dcf-relative">
-            <a 
-                class="url summary dcf-txt-decor-none"
-                href="<?php echo $frontend->getEventURL($context->getRawObject()) ?>">
-                <?php echo $savvy->dbStringtoHtml($context->event->displayTitle($context)) ?>
+    <?php if (isset($event_event_type) && !empty($event_event_type)): ?>
+        <small class="dcf-badge dcf-badge-roundrect">
+            <a href="<?php echo $frontend->getEventTypeURL() . '?q=' . $event_event_type->name; ?>" style="color: inherit;">
+                <?php echo $event_event_type->name; ?>
             </a>
-            <?php if (isset($event_event_type) && !empty($event_event_type)): ?>
-                <small class="dcf-badge dcf-badge-roundrect dcf-absolute dcf-top-0 dcf-left-100% dcf-ml-3">
-                    <a href="<?php echo $frontend->getEventTypeURL() . '?q=' . $event_event_type->name; ?>" style="color: inherit;">
-                        <?php echo $event_event_type->name; ?>
-                    </a>
-                </small>
-            <?php endif; ?>
-        </span>
+        </small>
+    <?php endif; ?>
+    <h2>
+        <a
+            class="url summary dcf-txt-decor-none"
+            href="<?php echo $frontend->getEventURL($context->getRawObject()) ?>">
+            <?php echo $savvy->dbStringtoHtml($context->event->displayTitle($context)) ?>
+        </a>
     </h2>
     <?php if (isset($context->event->subtitle)): ?><p class="dcf-subhead"><?php echo $savvy->dbStringtoHtml($context->event->subtitle) ?></p><?php endif; ?>
 <?php if (isset($context->event->subtitle)): ?></header><?php endif; ?>
