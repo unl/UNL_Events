b5b5034cb891be8ec84e6c1f297b6acef80de5a4
Thomas Neumann <thomasneumann21800@gmail.com>
2023-02-23 10:57:13 -0600

    Changed offset button displaying

--- www/templates/default/html/EventType.tpl.php
+++ www/templates/default/html/EventType.tpl.php
@@ -18,29 +18,29 @@
     <section id="updatecontent" class="day_cal dcf-col-100% dcf-col-67%-end@md">
         <?php echo $savvy->render($context, 'hcalendar/EventType.tpl.php'); ?>
 
-        <?php if($context->count() > 0): ?>
+        <?php
+            $previous_offset = $context->offset - $context->limit;
+            $next_offset     = $context->offset + $context->limit;
+
+            $previous_link = $context->getURL();
+            $next_link     = $context->getURL();
+
+            if ($context->limit != 100) {
+                $previous_link .= '&limit=' . $context->limit;
+                $next_link     .= '&limit=' . $context->limit;
+            }
+
+            if ($previous_offset > 0) {
+                $previous_link .= '&offset=' . $previous_offset;
+            }
+
+            if ($next_offset < $context->count()) {
+                $next_link .= '&offset=' . $next_offset;
+            }
+        ?>
+
+        <?php if($context->count() > 0 && !($previous_offset < 0 && $next_offset > $context->count())): ?>
             <div class="dcf-d-flex dcf-flex-row dcf-flex-nowrap dcf-jc-between dcf-ai-end dcf-mt-3">
-                <?php
-                    $previous_offset = $context->offset - $context->limit;
-                    $next_offset     = $context->offset + $context->limit;
-
-                    $previous_link = $context->getURL();
-                    $next_link     = $context->getURL();
-
-                    if ($context->limit != 100) {
-                        $previous_link .= '&limit=' . $context->limit;
-                        $next_link     .= '&limit=' . $context->limit;
-                    }
-
-                    if ($previous_offset > 0) {
-                        $previous_link .= '&offset=' . $previous_offset;
-                    }
-
-                    if ($next_offset < $context->count()) {
-                        $next_link .= '&offset=' . $next_offset;
-                    }
-                ?>
-
                 <?php if ($previous_offset < 0): ?>
                     <?php // We wanted to be able to disable this but you can not disable a link ?>
                     <button
@@ -76,7 +76,6 @@
                         Next <?php echo $context->limit; ?>
                     </a>
                 <?php endif; ?>
-
             </div>
         <?php endif; ?>
     </section>
