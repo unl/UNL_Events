fff77248c7003dc6e4bec58866ceae29bcfc4ae8
Thomas Neumann <57959550+tommyneu@users.noreply.github.com>
2023-03-21 13:39:09 -0500

    Fixed dcf-sticky on all pages that sidebar is used

--- www/templates/default/html/EventInstance.tpl.php
+++ www/templates/default/html/EventInstance.tpl.php
@@ -1,6 +1,8 @@
 <div class="dcf-grid dcf-col-gap-vw">
     <aside class="dcf-col-100% dcf-col-33%-start@md">
-        <?php echo $savvy->render($context, 'sidebar.tpl.php'); ?>
+        <div class="dcf-sticky dcf-pin-top">
+            <?php echo $savvy->render($context, 'sidebar.tpl.php'); ?>
+        </div>
     </aside>
     <section id="updatecontent" class="day_cal dcf-col-100% dcf-col-67%-end@md">
         <?php echo $savvy->render($context, 'hcalendar/EventInstance.tpl.php'); ?>
