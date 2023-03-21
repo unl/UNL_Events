6cbf3a538be343c9d6995fd5677a323d05064732
Thomas Neumann <57959550+tommyneu@users.noreply.github.com>
2023-03-21 16:42:46 -0500

    Changed styles

--- www/templates/default/hcalendar/Audience.tpl.php
+++ www/templates/default/hcalendar/Audience.tpl.php
@@ -1,9 +1,12 @@
 <div>
-    <p class="summary dcf-txt-xs unl-font-sans" aria-live="polite" aria-atomic="true">Displaying Audiences: 
-        <span><?php $context->getURL().'">'.$context->getFormattedAudiences(); ?></span>
+    <p class="dcf-txt-xs unl-font-sans unl-dark-gray">Displaying Audiences:
+        <span><?php echo $context->getFormattedAudiences(); ?></span>
     </p>
     <h2>Audience Results</h2>
-    <p><?php echo $context->count().' from all calendars matching the selected audiences'; ?></p>
+    <p class="dcf-txt-xs unl-font-sans unl-dark-gray">
+        <?php echo $context->count(); ?>
+        result(s) from all calendars matching the selected audience(s)
+    </p>
 </div>
 
 <?php echo $savvy->render($context, 'EventListing.tpl.php');
