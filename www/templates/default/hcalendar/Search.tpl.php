d690326d23567e8eef0ea31cad1c03e7da30d787
Thomas Neumann <57959550+tommyneu@users.noreply.github.com>
2023-03-22 13:52:05 -0500

    WIP search month widget integration

--- www/templates/default/hcalendar/Search.tpl.php
+++ www/templates/default/hcalendar/Search.tpl.php
@@ -27,7 +27,12 @@
             </span>
         <?php endif; ?>
     </p>
-    <h2 class="dcf-mt-0" id="heading-date">Search Results</h2>
+    <?php if ($dt = $context->getSearchTimestamp()): ?>
+        <h2 class="dcf-mt-0" id="heading-date" data-datetime="<?php echo date(DATE_ATOM, $dt); ?>">
+    <?php else: ?>
+        <h2 class="dcf-mt-0" id="heading-date">Search Results</h2>
+    <?php endif; ?>
+
     <p class="dcf-txt-xs unl-font-sans unl-dark-gray">
         <?php echo $context->count(); ?>
 
