20dd145cc54d5233fead2cbc3b2a49e243cfd4c3
Thomas Neumann <57959550+tommyneu@users.noreply.github.com>
2023-03-23 09:12:53 -0500

    WIP next button offset

--- www/templates/default/html/prev_next_buttons.tpl.php
+++ www/templates/default/html/prev_next_buttons.tpl.php
@@ -14,12 +14,12 @@
         $previous_link .= '&offset=' . $previous_offset;
     }
 
-    if ($next_offset < $context->count() - 1) {
+    if ($next_offset <= $context->count()) {
         $next_link .= '&offset=' . $next_offset;
     }
 ?>
 
-<?php if($context->count() > 0 && !($previous_offset < 0 && $next_offset > $context->count() - 1)): ?>
+<?php if($context->count() > 0 && !($previous_offset < 0 && $next_offset > $context->count())): ?>
     <div class="dcf-d-flex dcf-flex-row dcf-flex-nowrap dcf-jc-between dcf-ai-end dcf-mt-3">
         <?php if ($previous_offset < 0): ?>
             <?php // We wanted to be able to disable this but you can not disable a link ?>
@@ -40,7 +40,7 @@
 
         <p class="dcf-txt-xs">Only Displaying <?php echo $context->limit; ?> Results at a time</p>
 
-        <?php if ($next_offset > $context->count() - 1): ?>
+        <?php if ($next_offset > $context->count()): ?>
             <?php // We wanted to be able to disable this but you can not disable a link ?>
             <button
                 class="dcf-btn dcf-btn-secondary"
