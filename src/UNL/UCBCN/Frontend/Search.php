e743ecccc445349c55de17f8d9b3c721d7d57af6
Thomas Neumann <57959550+tommyneu@users.noreply.github.com>
2023-03-24 15:38:21 -0500

    Added missing format

--- src/UNL/UCBCN/Frontend/Search.php
+++ src/UNL/UCBCN/Frontend/Search.php
@@ -138,7 +138,7 @@ class Search extends EventListing implements RoutableInterface
                 $sql .= 'IF (recurringdate.recurringdate IS NULL,
                     e.starttime,
                     CONCAT(DATE_FORMAT(recurringdate.recurringdate,"%Y-%m-%d"),DATE_FORMAT(e.starttime," %H:%i:%s"))
-                ) >= STR_TO_DATE(\'' . date('Y-m-d', $this->date_parser->start_date) . '\')
+                ) >= STR_TO_DATE(\'' . date('Y-m-d', $this->date_parser->start_date) . '\', \'%Y-%m-%d\')
                 AND
                 IF (recurringdate.recurringdate IS NULL,
                     e.endtime,
