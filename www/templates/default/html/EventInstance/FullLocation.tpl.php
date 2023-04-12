208002bdcf13fb44806ebceb7fc9ce11772602b0
Thomas Neumann <thomasneumann21800@gmail.com>
2023-04-12 09:56:11 -0500

    Fix location output

--- www/templates/default/html/EventInstance/FullLocation.tpl.php
+++ www/templates/default/html/EventInstance/FullLocation.tpl.php
@@ -16,36 +16,38 @@ $location_additional_info = !empty($context->eventdatetime->location_additionalp
                 <path d="M12 0C7.589 0 4 3.589 4 8c0 4.245 7.273 15.307 7.583 15.775a.497.497 0 00.834 0C12.727 23.307 20 12.245 20 8c0-4.411-3.589-8-8-8zm0 22.58C10.434 20.132 5 11.396 5 8c0-3.86 3.14-7 7-7s7 3.14 7 7c0 3.395-5.434 12.132-7 14.58z"></path>
                 <path d="M12 4.5c-1.93 0-3.5 1.57-3.5 3.5s1.57 3.5 3.5 3.5 3.5-1.57 3.5-3.5-1.57-3.5-3.5-3.5zm0 6c-1.378 0-2.5-1.122-2.5-2.5s1.122-2.5 2.5-2.5 2.5 1.122 2.5 2.5-1.122 2.5-2.5 2.5z"></path>
             </svg>
-        <?php if (isset($location->mapurl) && filter_var($location->mapurl, FILTER_VALIDATE_URL)): ?>
-            <a class="mapurl" href="<?php echo $location->mapurl ?>"><?php echo $location->name; ?></a>
-        <?php elseif (isset($location->webpageurl) && filter_var($location->webpageurl, FILTER_VALIDATE_URL)): ?>
-            <a class="webpageurl" href="<?php echo $location->webpageurl ?>"><?php echo $location->name; ?></a>
-        <?php else: ?>
-            <?php echo $location->name; ?>
-        <?php endif; ?>
-        <?php if (!empty($room)): ?>
-            <span class="room">Room: <?php echo $room ?></span>
-        <?php endif; ?>
-        <?php if (isset($location->streetaddress1)): ?>
-            <div class="adr">
-                <span class="street-address"><?php echo $savvy->dbStringtoHtml($location->streetaddress1 . "\n" . $location->streetaddress2) ?></span>
-                <?php if (isset($location->city)): ?>
-                    <span class="locality"><?php echo $savvy->dbStringtoHtml($location->city) ?></span>
+            <span>
+                <?php if (isset($location->mapurl) && filter_var($location->mapurl, FILTER_VALIDATE_URL)): ?>
+                    <a class="mapurl" href="<?php echo $location->mapurl ?>"><?php echo $location->name; ?></a>
+                <?php elseif (isset($location->webpageurl) && filter_var($location->webpageurl, FILTER_VALIDATE_URL)): ?>
+                    <a class="webpageurl" href="<?php echo $location->webpageurl ?>"><?php echo $location->name; ?></a>
+                <?php else: ?>
+                    <?php echo $location->name; ?>
                 <?php endif; ?>
-                <?php if (isset($location->state)): ?>
-                    <span class="region"><?php echo $savvy->dbStringtoHtml($location->state) ?></span>
+                <?php if (!empty($room)): ?>
+                    <span class="room">Room: <?php echo $room ?></span>
                 <?php endif; ?>
-                <?php if (isset($location->zip)): ?>
-                    <span class="postal-code"><?php echo $savvy->dbStringtoHtml($location->zip) ?></span>
+                <?php if (isset($location->streetaddress1)): ?>
+                    <div class="adr">
+                        <span class="street-address"><?php echo $savvy->dbStringtoHtml($location->streetaddress1 . "\n" . $location->streetaddress2) ?></span>
+                        <?php if (isset($location->city)): ?>
+                            <span class="locality"><?php echo $savvy->dbStringtoHtml($location->city) ?></span>
+                        <?php endif; ?>
+                        <?php if (isset($location->state)): ?>
+                            <span class="region"><?php echo $savvy->dbStringtoHtml($location->state) ?></span>
+                        <?php endif; ?>
+                        <?php if (isset($location->zip)): ?>
+                            <span class="postal-code"><?php echo $savvy->dbStringtoHtml($location->zip) ?></span>
+                        <?php endif; ?>
+                    </div>
                 <?php endif; ?>
-            </div>
-        <?php endif; ?>
-        <?php if (!empty($directions)): ?>
-            <div class="directions">Directions: <?php echo $savvy->dbStringtoHtml($directions) ?></div>
-        <?php endif ?>
-        <?php if (!empty($location_additional_info)): ?>
-            <div class="additionalinfo">Additional Info: <?php echo $savvy->dbStringtoHtml($location_additional_info) ?></div>
-        <?php endif ?>
+                <?php if (!empty($directions)): ?>
+                    <div class="directions">Directions: <?php echo $savvy->dbStringtoHtml($directions) ?></div>
+                <?php endif ?>
+                <?php if (!empty($location_additional_info)): ?>
+                    <div class="additionalinfo">Additional Info: <?php echo $savvy->dbStringtoHtml($location_additional_info) ?></div>
+                <?php endif ?>
+            </span>
         </div>
     <?php endif; ?>
 <?php endif; ?>
@@ -63,10 +65,12 @@ $location_additional_info = !empty($context->eventdatetime->location_additionalp
             <path d="M12,16.5c-0.551,0-1,0.448-1,1s0.449,1,1,1s1-0.448,1-1S12.551,16.5,12,16.5z M12,17.5L12,17.5h0.5H12z"></path>
             <g><path fill="none" d="M0 0H24V24H0z"></path></g>
         </svg>
-        <span class="dcf-sr-only">Virtual Location:</span>
-        <a href="<?php echo $webcast->url; ?>"><?php echo $webcast->title; ?></a>
-        <?php if (isset($webcast_additional_info) && !empty($webcast_additional_info)): ?>
-            <div class="additionalinfo">Additional Info: <?php echo $savvy->dbStringtoHtml($webcast_additional_info) ?></div>
-        <?php endif ?>
+        <span>
+            <span class="dcf-sr-only">Virtual Location:</span>
+            <a href="<?php echo $webcast->url; ?>"><?php echo $webcast->title; ?></a>
+            <?php if (isset($webcast_additional_info) && !empty($webcast_additional_info)): ?>
+                <div class="additionalinfo">Additional Info: <?php echo $savvy->dbStringtoHtml($webcast_additional_info) ?></div>
+            <?php endif ?>
+        </span>
     </div>
 <?php endif; ?>
--- www/templates/default/html/EventInstance/Location.tpl.php
+++ www/templates/default/html/EventInstance/Location.tpl.php
@@ -6,14 +6,16 @@
                 <path d="M12 0C7.589 0 4 3.589 4 8c0 4.245 7.273 15.307 7.583 15.775a.497.497 0 00.834 0C12.727 23.307 20 12.245 20 8c0-4.411-3.589-8-8-8zm0 22.58C10.434 20.132 5 11.396 5 8c0-3.86 3.14-7 7-7s7 3.14 7 7c0 3.395-5.434 12.132-7 14.58z"></path>
                 <path d="M12 4.5c-1.93 0-3.5 1.57-3.5 3.5s1.57 3.5 3.5 3.5 3.5-1.57 3.5-3.5-1.57-3.5-3.5-3.5zm0 6c-1.378 0-2.5-1.122-2.5-2.5s1.122-2.5 2.5-2.5 2.5 1.122 2.5 2.5-1.122 2.5-2.5 2.5z"></path>
             </svg>
-            <span class="dcf-sr-only">Location:</span>
-            <?php if (isset($l->mapurl) && filter_var($l->mapurl, FILTER_VALIDATE_URL)): ?>
-                <a class="mapurl" href="<?php echo $l->mapurl ?>"><?php echo $l->name; ?></a>
-            <?php elseif (isset($l->webpageurl) && filter_var($l->webpageurl, FILTER_VALIDATE_URL)): ?>
-                <a class="webpageurl" href="<?php echo $l->webpageurl ?>"><?php echo $l->name; ?></a>
-            <?php else: ?>
-                <?php echo $l->name; ?>
-            <?php endif; ?>
+            <span>
+                <span class="dcf-sr-only">Location:</span>
+                <?php if (isset($l->mapurl) && filter_var($l->mapurl, FILTER_VALIDATE_URL)): ?>
+                    <a class="mapurl" href="<?php echo $l->mapurl ?>"><?php echo $l->name; ?></a>
+                <?php elseif (isset($l->webpageurl) && filter_var($l->webpageurl, FILTER_VALIDATE_URL)): ?>
+                    <a class="webpageurl" href="<?php echo $l->webpageurl ?>"><?php echo $l->name; ?></a>
+                <?php else: ?>
+                    <?php echo $l->name; ?>
+                <?php endif; ?>
+            </span>
         </span>
     <?php endif; ?>
 <?php endif; ?>
@@ -26,7 +28,9 @@
             <path d="M12,16.5c-0.551,0-1,0.448-1,1s0.449,1,1,1s1-0.448,1-1S12.551,16.5,12,16.5z M12,17.5L12,17.5h0.5H12z"></path>
             <g><path fill="none" d="M0 0H24V24H0z"></path></g>
         </svg>
-        <span class="dcf-sr-only">Virtual Location:</span>
-        <a href="<?php echo $webcast->url; ?>"><?php echo $webcast->title; ?></a>
+        <span>
+            <span class="dcf-sr-only">Virtual Location:</span>
+            <a href="<?php echo $webcast->url; ?>"><?php echo $webcast->title; ?></a>
+        </span>
     </div>
 <?php endif; ?>
