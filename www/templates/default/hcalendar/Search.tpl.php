eef4618dc138c5d8d96aba031babaf2503d7037f
Thomas Neumann <thomasneumann21800@gmail.com>
2023-05-11 10:24:39 -0500

    Draft: Added icons

--- www/templates/default/hcalendar/Search.tpl.php
+++ www/templates/default/hcalendar/Search.tpl.php
@@ -26,12 +26,30 @@
 
         <?php if (!empty($context->search_event_type)): ?>
             <span class='dcf-d-block dcf-ml-2'>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" class="dcf-h-4 dcf-w-4 dcf-fill-current">
+                    <path d="M29.9,15.7c0,0,0-0.1-0.1-0.2c-0.1-0.2-0.2-0.4-0.4-0.6l-7.2-7.2c-0.7-0.7-1.7-0.7-2.4,0c-0.7,0.7-0.7,1.7,0,2.4l4.4,4.3
+                    H3.4V1.7C3.4,0.8,2.6,0,1.7,0S0,0.8,0,1.7v14.4c0,0.9,0.8,1.7,1.7,1.7h22.5l-4.4,4.3c-0.3,0.3-0.5,0.7-0.5,1.2s0.2,0.9,0.5,1.2
+                    c0.3,0.3,0.7,0.5,1.2,0.5s0.9-0.2,1.2-0.5l7.2-7.2c0.1-0.1,0.3-0.3,0.4-0.5c0-0.1,0.1-0.2,0.1-0.3c0-0.1,0-0.2,0-0.4
+                    C30,16,30,15.8,29.9,15.7z"/>
+                    <g>
+                        <path fill="none" d="M0,0h30v30H0V0z"/>
+                    </g>
+                </svg>
                 Event Type: <?php echo $context->search_event_type; ?>
             </span>
         <?php endif; ?>
 
         <?php if (!empty($context->search_event_audience)): ?>
             <span class='dcf-d-block dcf-ml-2'>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" class="dcf-h-4 dcf-w-4 dcf-fill-current">
+                    <path d="M29.9,15.7c0,0,0-0.1-0.1-0.2c-0.1-0.2-0.2-0.4-0.4-0.6l-7.2-7.2c-0.7-0.7-1.7-0.7-2.4,0c-0.7,0.7-0.7,1.7,0,2.4l4.4,4.3
+                    H3.4V1.7C3.4,0.8,2.6,0,1.7,0S0,0.8,0,1.7v14.4c0,0.9,0.8,1.7,1.7,1.7h22.5l-4.4,4.3c-0.3,0.3-0.5,0.7-0.5,1.2s0.2,0.9,0.5,1.2
+                    c0.3,0.3,0.7,0.5,1.2,0.5s0.9-0.2,1.2-0.5l7.2-7.2c0.1-0.1,0.3-0.3,0.4-0.5c0-0.1,0.1-0.2,0.1-0.3c0-0.1,0-0.2,0-0.4
+                    C30,16,30,15.8,29.9,15.7z"/>
+                    <g>
+                        <path fill="none" d="M0,0h30v30H0V0z"/>
+                    </g>
+                </svg>
                 Target Audience: <?php echo $context->search_event_audience; ?>
             </span>
         <?php endif; ?>
