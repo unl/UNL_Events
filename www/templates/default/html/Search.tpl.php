621bc90b016d83ae24d14eff0ba95ef65d759fb2
Thomas Neumann <thomasneumann21800@gmail.com>
2023-05-23 14:44:31 -0500

    Added logic for loading filters client side

--- www/templates/default/html/Search.tpl.php
+++ www/templates/default/html/Search.tpl.php
@@ -12,4 +12,26 @@
         <?php echo $savvy->render($context, 'hcalendar/Search.tpl.php'); ?>
         <?php echo $savvy->render($context, 'prev_next_buttons.tpl.php'); ?>
     </section>
+    <template id="loadingContent">
+        <div id="results">
+            <p class="dcf-txt-xs unl-dark-gray">
+                <span class="dcf-bold">Loading Search</span>
+            </p>
+            <h1 class="dcf-txt-h3 dcf-mt-0" id="heading-date">Search Results</h1>
+            <p class="dcf-txt-xs unl-dark-gray">
+                Loading Results
+            </p>
+        </div>
+        <div class='dcf-d-flex dcf-jc-center dcf-ai-center dcf-h-12'>
+            <div class='dcf-progress-spinner'></div>
+        </div>
+    </template>
+    <template id="errorContent">
+        <div id="results">
+            <h1 class="dcf-txt-h3 dcf-mt-0" id="heading-date">Search Results</h1>
+            <p class="dcf-txt-xs unl-dark-gray">
+                Error loading results, please try again later.
+            </p>
+        </div>
+    </template>
 </div>
--- www/templates/default/html/Upcoming.tpl.php
+++ www/templates/default/html/Upcoming.tpl.php
@@ -19,4 +19,26 @@
         ?>
         <?php echo $savvy->render($context, 'hcalendar/Upcoming.tpl.php'); ?>
     </section>
+    <template id="loadingContent">
+        <div id="results">
+            <p class="dcf-txt-xs unl-dark-gray">
+                <span class="dcf-bold">Loading Upcoming</span>
+            </p>
+            <h1 class="dcf-txt-h3 dcf-mt-0" id="heading-date">Upcoming Events</h1>
+            <p class="dcf-txt-xs unl-dark-gray">
+                Loading Results
+            </p>
+        </div>
+        <div class='dcf-d-flex dcf-jc-center dcf-ai-center dcf-h-12'>
+            <div class='dcf-progress-spinner'></div>
+        </div>
+    </template>
+    <template id="errorContent">
+        <div id="results">
+            <h1 class="dcf-txt-h3 dcf-mt-0" id="heading-date">Upcoming Results</h1>
+            <p class="dcf-txt-xs unl-dark-gray">
+                Error loading results, please try again later.
+            </p>
+        </div>
+    </template>
 </div>
