0c477a729854d1f78280d81c90dc35e6858eefce
Thomas Neumann <thomasneumann21800@gmail.com>
2023-05-23 14:44:01 -0500

    Fix partials to not load unneeded items

--- www/templates/default/html/EventType.tpl.php
+++ www/templates/default/html/EventType.tpl.php
@@ -1,10 +1,12 @@
 <div class="dcf-grid dcf-col-gap-vw dcf-row-gap-6">
-    <section class="dcf-col-100% dcf-col-33%-start@md">
-        <nav>
-            <a class="dcf-show-on-focus" href="#updatecontent">Skip Sidebar</a>
-        </nav>
-        <?php echo $savvy->render($context, 'filters.tpl.php'); ?>
-    </section>
+    <?php if($context->options['format'] !== "partial"): ?>
+        <aside class="dcf-col-100% dcf-col-33%-start@md">
+            <nav>
+                <a class="dcf-show-on-focus" href="#updatecontent">Skip Sidebar</a>
+            </nav>
+            <?php echo $savvy->render($context, 'filters.tpl.php'); ?>
+        </aside>
+    <?php endif; ?>
     <section id="updatecontent" class="day_cal dcf-col-100% dcf-col-67%-end@md">
         <?php echo $savvy->render($context, 'hcalendar/EventType.tpl.php'); ?>
 
--- www/templates/default/html/Upcoming.tpl.php
+++ www/templates/default/html/Upcoming.tpl.php
@@ -1,11 +1,13 @@
 <div class="dcf-grid dcf-col-gap-vw dcf-row-gap-6">
-    <aside class="dcf-col-100% dcf-col-33%-start@md">
-        <nav>
-            <a class="dcf-show-on-focus" href="#updatecontent">Skip Sidebar</a>
-        </nav>
-        <?php echo $savvy->render($context, 'sidebar.tpl.php'); ?>
-        <?php echo $savvy->render($context, 'filters.tpl.php'); ?>
-    </aside>
+    <?php if($context->options['format'] !== "partial"): ?>
+        <aside class="dcf-col-100% dcf-col-33%-start@md">
+            <nav>
+                <a class="dcf-show-on-focus" href="#updatecontent">Skip Sidebar</a>
+            </nav>
+            <?php echo $savvy->render($context, 'sidebar.tpl.php'); ?>
+            <?php echo $savvy->render($context, 'filters.tpl.php'); ?>
+        </aside>
+    <?php endif; ?>
     <section id="updatecontent" class="day_cal dcf-col-100% dcf-col-67%-end@md">
         <?php
             if ($context->isHomepage()) {
