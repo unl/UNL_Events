64228dbe31c90b3c1b1b6c31ebeb22eaa428338c
Thomas Neumann <thomasneumann21800@gmail.com>
2023-06-12 16:15:53 -0500

    Preliminary Calendar Location editor

--- src/UNL/UCBCN/Manager/CalendarLocation.php
+++ src/UNL/UCBCN/Manager/CalendarLocation.php
@@ -1,28 +1,127 @@
 <?php
 namespace UNL\UCBCN\Manager;
 
+use Exception;
 use UNL\UCBCN\Calendar;
 use UNL\UCBCN\Permission;
-use UNL\UCBCN as BaseUCBCN;
+use UNL\UCBCN\Location as Location;
 
 class CalendarLocation extends PostHandler
 {
     public $options = array();
+    public $post;
     public $calendar;
 
     public function __construct($options = array())
     {
         $this->options = $options + $this->options;
+        $this->calendar = Calendar::getByShortName($this->options['calendar_shortname']);
+    }
 
-        # check if we are looking to edit a calendar
-        if (array_key_exists('calendar_shortname', $this->options)) {
-            $this->calendar = Calendar::getByShortname($this->options['calendar_shortname']);
-        }
+    public function getCalendarLocations() 
+    {
+        return LocationUtility::getCalendarLocations($this->calendar->id);
+    }
+
+    public function getUserCalendars() 
+    {
+        $user = Auth::getCurrentUser();
+
+        return $user->getCalendars();
     }
 
     public function handlePost(array $get, array $post, array $files)
     {
-        # redirect
-        return $this->calendar->getManageURL();
+        $method = $post['method'] ?? "";
+        try {
+            switch ($method) {
+                case "post":
+                    $this->create_location($post);
+                    break;
+                case "put":
+                    $this->update_location($post);
+                    break;
+                case "delete":
+                    $this->detach_location($post);
+                    break;
+                default: 
+                    throw new ValidationException('Invalid Method');
+            }
+
+        } catch (ValidationException $e) {
+            $this->post = $post;
+            $this->flashNotice(parent::NOTICE_LEVEL_ALERT, 'Sorry! We couldn\'t create your event', $e->getMessage());
+            throw $e;
+        }
+
+        switch ($method) {
+            case "post":
+                $this->flashNotice(parent::NOTICE_LEVEL_SUCCESS, 'Location Created', 'Your calendar\'s location has been created.');
+                break;
+            case "put":
+                $this->flashNotice(parent::NOTICE_LEVEL_SUCCESS, 'Location Updated', 'Your calendar\'s location has been updated.');
+                break;
+            case "delete":
+                $this->flashNotice(parent::NOTICE_LEVEL_SUCCESS, 'Location Detached', 'The location has been detached from your calendar.');
+                break;
+        }
+
+        //redirect
+        return $this->calendar->getLocationURL();
+    }
+
+    private function create_location(array $post_data)
+    {
+        $user = Auth::getCurrentUser();
+
+        $post_data['location_save_calendar'] = 'on';
+        $calendar = $this->calendar;
+
+        $this->validateLocation($post_data);
+
+        LocationUtility::addLocation($post_data, $user, $calendar);
+    }
+
+    private function update_location(array $post_data)
+    {
+        $user = Auth::getCurrentUser();
+
+        $post_data['location_save_calendar'] = 'on';
+        $calendar = $this->calendar;
+
+        if (!empty($post_data['location']) && $post_data['location'] === "New") {
+            throw new ValidationException('Missing Location To Update');
+        }
+
+        $this->validateLocation($post_data);
+
+        try {
+            LocationUtility::updateLocation($post_data, $user, $calendar);
+        } catch(Exception $e) {
+            throw new ValidationException('Error Updating Location');
+        }
+    }
+
+    private function detach_location(array $post_data)
+    {
+        if (!empty($post_data['location']) && $post_data['location'] === "New") {
+            throw new ValidationException('Missing Location To Detach');
+        }
+
+        $location = Location::getByID($post_data['location']);
+        if ($location === null) {
+            throw new ValidationException('Invalid Location ID');
+        }
+
+        $location->calendar_id = null;
+        $location->update();
+    }
+
+    private function validateLocation(array $post_data)
+    {
+        $validate_data = LocationUtility::validateLocation($post_data);
+        if (!$validate_data['valid']) {
+            throw new ValidationException($validate_data['message']);
+        }
     }
 }
--- www/templates/default/html/manager/CalendarLocation.tpl.php
+++ www/templates/default/html/manager/CalendarLocation.tpl.php
@@ -3,8 +3,224 @@
     $crumbs->crumbs = array(
         "Events Manager" => "/manager",
         $context->calendar->name => $context->calendar->getManageURL(),
-        "Calendar Location" => null
+        "Calendar Locations" => null
     );
     echo $savvy->render($crumbs, 'BreadcrumbBar.tpl.php');
+
+    $post = $context->post ?? null;
+
+    $token_inputs = '<input ' .
+        'type="hidden" '.
+        'name="' . $controller->getCSRFHelper()->getTokenNameKey() . '" ' .
+        'value="' . $controller->getCSRFHelper()->getTokenName() . '" ' .
+    '> ' .
+    '<input ' .
+        'type="hidden"' .
+        'name="' . $controller->getCSRFHelper()->getTokenValueKey() . '" ' .
+        'value="' . $controller->getCSRFHelper()->getTokenValue() . '" ' .
+    '> ';
 ?>
-<h1>Calendar Locations</h1>
+<script>
+    const LOCATIONS = [];
+    LOCATIONS[0] = {
+        'location'                        : 'new',
+        'location-name'                   : '',
+        'location-address-1'              : '',
+        'location-address-2'              : '',
+        'location-city'                   : '',
+        'location-state'                  : 'NE',
+        'location-zip'                    : '',
+        'location-map-url'                : '',
+        'location-webpage'                : '',
+        'location-hours'                  : '',
+        'location-phone'                  : '',
+        'location-room'                   : '',
+        'location-directions'             : '',
+        'location-additional-public-info' : '',
+        'user_id'                         : '',
+        'calendar_id'                     : '',
+    };
+</script>
+
+<div class="dcf-mb-5 dcf-d-flex dcf-flex-row dcf-flex-wrap dcf-jc-between dcf-ai-center">
+    <h1 id="table_desc">Calendar Locations</h1>
+    <button id="new_location" class="dcf-btn dcf-btn-primary" type="button">Create A New Location</button>
+</div>
+
+<table class="dcf-table dcf-table-responsive dcf-table-striped dcf-w-100%" aria-describedby="table_desc">
+    <thead>
+        <tr>
+            <th>Location Name</th>
+            <th>Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($context->getCalendarLocations() as $location): ?>
+            <tr>
+                <td>
+                    <?php echo $location->name; ?>
+                    <script>
+                        <?php $location_json = $location->toJSON(); ?>
+                        <?php $raw_location_json = $savvy->getRawObject($location_json); ?>
+                        LOCATIONS[<?php echo $location->id; ?>] = <?php
+                            echo json_encode($raw_location_json, JSON_UNESCAPED_SLASHES);
+                        ?>;
+                    </script>
+                </td>
+                <td>
+                    <form id="location_delete_<?php echo $location->id; ?>" method="post">
+                        <?php echo $token_inputs; ?>
+
+                        <input type="hidden" name="location" value="<?php echo $location->id; ?>">
+                        <input type="hidden" name="method" value="delete">
+                    </form>
+                    <div class="dcf-btn-group">
+                        <button
+                            class="dcf-btn dcf-btn-primary events-modify-location"
+                            type="button"
+                            data-location-id="<?php echo $location->id; ?>"
+                        >
+                            Modify
+                        </button>
+                        <input
+                            class="dcf-btn dcf-btn-secondary"
+                            type="submit"
+                            value="Detach"
+                            form="location_delete_<?php echo $location->id; ?>"
+                        >
+                    </div>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+
+<form
+    id="createOrModify"
+    class="dcf-form
+        dcf-mt-5
+        <?php if (!isset($post)): ?>
+            dcf-d-none
+        <?php endif; ?>
+    "
+    method="post"
+>
+    <?php echo $token_inputs; ?>
+    <input
+        type="hidden"
+        id="location"
+        name="location"
+        <?php if (isset($post['location'])): ?>
+            value="<?php echo $post['location'];?>"
+        <?php else: ?>
+            value="new"
+        <?php endif; ?>
+    >
+    <input
+        type="hidden"
+        id="createOrModify-method"
+        name="method"
+        <?php if (isset($post['method'])): ?>
+            value="<?php echo $post['method'];?>"
+        <?php else: ?>
+            value="post"
+        <?php endif; ?>
+    >
+
+    <fieldset class="dcf-mt-6" id="new-location-fields">
+        <legend id="createOrModify-legend">Create New Location</legend>
+
+        <?php echo $savvy->render($post, 'PhysicalLocationForm.tpl.php'); ?>
+
+        <div class="dcf-form-group">
+            <div class="dcf-input-checkbox">
+                <input
+                    id="location-save"
+                    name="location_save"
+                    type="checkbox"
+                    <?php
+                        if (isset($post['location_save']) && $post['location_save'] == 'on') { 
+                            echo CHECKED_INPUT; 
+                        }
+                    ?>
+                >
+                <label for="location-save">Save this location for your future events</label>
+            </div>
+        </div>
+
+        <div class="dcf-form-group">
+            <input id="createOrModify-submit" class="dcf-btn dcf-btn-primary" type="submit" value="Create New Location">
+            <button id="createOrModify-cancel" class="dcf-btn dcf-btn-secondary" type="button">Cancel</button>
+        </div>
+    </fieldset>
+</form>
+
+
+<script>
+    const createButton = document.getElementById('new_location');
+    const modifyButtons = document.querySelectorAll('.events-modify-location');
+
+    const createOrModifyForm = document.getElementById('createOrModify');
+    const createOrModifyLegend = document.getElementById('createOrModify-legend');
+    const createOrModifySubmit = document.getElementById('createOrModify-submit');
+    const createOrModifyCancel = document.getElementById('createOrModify-cancel');
+    const createOrModifyMethod = document.getElementById('createOrModify-method');
+
+    createOrModifyCancel.addEventListener('click', () => {
+        if (!confirm('Are you sure you want to clear out the form?')) {
+            return;
+        }
+        hideForm();
+    });
+
+    createButton.addEventListener('click', () => {
+        if (!createOrModifyForm.classList.contains('dcf-d-none')) {
+            if (!confirm('Are you sure you want to clear out the form?')) {
+                return;
+            }
+        }
+        createOrModifyLegend.innerText = "Create New Location";
+        createOrModifySubmit.value = "Create New Location";
+        createOrModifyMethod.value = "post";
+        fillInputs(0);
+        showForm();
+    });
+
+    modifyButtons.forEach((modifyButton) => {
+        modifyButton.addEventListener('click', () => {
+            if (!createOrModifyForm.classList.contains('dcf-d-none')) {
+                if (!confirm('Are you sure you want to clear out the form?')) {
+                    return;
+                }
+            }
+            createOrModifyLegend.innerText = "Modify Location";
+            createOrModifySubmit.value = "Modify Location";
+            createOrModifyMethod.value = "put";
+            fillInputs(modifyButton.dataset.locationId);
+            showForm();
+        });
+    });
+
+    function fillInputs(location_id) {
+        const locationToModify = LOCATIONS[location_id];
+        const specialProps = ['user_id', 'calendar_id'];
+
+        for (const locationProp in locationToModify) {
+            if (specialProps.includes(locationProp)) { continue; }
+
+            const elementToModify = document.getElementById(locationProp);
+            if (elementToModify === null) { throw new Error(`Missing Element ${locationProp}`); }
+
+            const defaultValue = locationProp === 'location-state' ? "NE" : "";
+            elementToModify.value = locationToModify[locationProp] ?? defaultValue;
+        }
+    }
+
+    function hideForm() {
+        createOrModifyForm.classList.add('dcf-d-none');
+    }
+
+    function showForm() {
+        createOrModifyForm.classList.remove('dcf-d-none');
+    }
+</script>
