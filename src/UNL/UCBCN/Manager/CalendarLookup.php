a1c4387c598ab3104db0592d93065a740e927517
Thomas Neumann <thomasneumann21800@gmail.com>
2023-06-16 10:21:08 -0500

    Added functionality to calendar lookup

--- src/UNL/UCBCN/Manager/CalendarLookup.php
+++ src/UNL/UCBCN/Manager/CalendarLookup.php
@@ -1,24 +1,55 @@
 <?php
 namespace UNL\UCBCN\Manager;
 
-use UNL\UCBCN\Account;
+use UNL\UCBCN\Calendar as Calendar;
+use UNL\UCBCN\Permissions;
 
 class CalendarLookup extends PostHandler
 {
     public $options = array();
-    public $account;
+    public $post;
+    public $calendar;
+    
 
     public function __construct($options = array())
     {
         $this->options = $options + $this->options;
+    }
 
-        $user = Auth::getCurrentUser();
-        $this->account = $user->getAccount();
+    public function getUsers()
+    {
+        if (!isset($this->calendar) || $this->calendar === false) {
+            return array();
+        }
+
+        return $this->calendar->getUsers();
+    }
+
+    public function getUserPermissions(string $user_id)
+    {
+        if (!isset($this->calendar) || $this->calendar === false) {
+            return array();
+        }
+
+        $options = array(
+            'user_uid' => $user_id,
+            'calendar_id' => $this->calendar->id,
+        );
+
+        return new Permissions($options);
     }
 
     public function handlePost(array $get, array $post, array $files)
     {
+        $this->post = $post;
+
+        $this->calendar = Calendar::getByShortName($this->post['lookupTerm']);
+
+        if ($this->calendar === false) {
+            $this->flashNotice(parent::NOTICE_LEVEL_ALERT, 'Calendar Not Found', 'We could not find a calendar matching your search.');
+        }
+    
         //redirect
-        return Controller::getUserLocationURL();
+        return null;
     }
 }
--- www/templates/default/html/manager/CalendarLookup.tpl.php
+++ www/templates/default/html/manager/CalendarLookup.tpl.php
@@ -5,5 +5,53 @@
         "Calendar Lookup" => null
     );
     echo $savvy->render($crumbs, 'BreadcrumbBar.tpl.php');
+
+    $token_inputs = '<input ' .
+        'type="hidden" '.
+        'name="' . $controller->getCSRFHelper()->getTokenNameKey() . '" ' .
+        'value="' . $controller->getCSRFHelper()->getTokenName() . '" ' .
+    '> ' .
+    '<input ' .
+        'type="hidden"' .
+        'name="' . $controller->getCSRFHelper()->getTokenValueKey() . '" ' .
+        'value="' . $controller->getCSRFHelper()->getTokenValue() . '" ' .
+    '> ';
 ?>
 <h1>Calendar Lookup</h1>
+
+<div class="dcf-grid-halves@md">
+    <form class="dcf-form" method="post">
+        <?php echo $token_inputs; ?>
+        <label for="lookupTerm">Calendar Shortname</label>
+        <div class="dcf-input-group">
+            <input id="lookupTerm" name="lookupTerm" type="text" value="<?php echo $context->post['lookupTerm'] ?? ""; ?>" required="">
+            <button class="dcf-btn dcf-btn-primary" id="lookup-submit" name="submit" type="submit">Search</button>
+        </div>
+        <span class="dcf-form-help">
+            <span class="dcf-d-inline-block">
+                This shortname can be found by navigating to the calendar's page and copying the value from the URL.
+            </span>
+            <span class="dcf-d-inline-block">
+                To look <?php echo UNL\UCBCN\Frontend\Controller::$url; ?>wdn/, search for wdn. This search is case sensitive.
+            </span>
+        </span>
+    </form>
+</div>
+
+<?php if (isset($context->calendar) && $context->calendar !== false): ?>
+    <div class="dcf-mt-5">
+        <h2>Users on the '<?php echo $context->calendar->name; ?>' calendar</h2>
+        <div class="dcf-grid-thirds@lg dcf-grid-halves@sm dcf-row-gap-4 dcf-col-gap-vw">
+            <?php foreach($context->getUsers() as $user): ?>
+                <div>
+                    <p class="dcf-bold dcf-mb-0"><?php echo $user->uid; ?></p>
+                    <ul>
+                        <?php foreach($context->getUserPermissions($user->uid) as $permission): ?>
+                            <li class="dcf-txt-sm dcf-mb-0"><?php echo $permission->name; ?></li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+<?php endif; ?>
