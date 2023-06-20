47b127a973cf6b6714de111054ee436fc2bd0e4f
Thomas Neumann <thomasneumann21800@gmail.com>
2023-06-20 10:04:37 -0500

    Updated meta with more empty results

--- src/UNL/UCBCN/Frontend/MetaTagUtility.php
+++ src/UNL/UCBCN/Frontend/MetaTagUtility.php
@@ -19,34 +19,25 @@ class MetaTagUtility {
     public function getMetaTags(): string
     {
         $metaTagOutput = "";
-    
+
         $metaTagOutput .= '<meta name="description" content="' . $this->description . '">' . PHP_EOL;
 
         $metaTagOutput .= '<meta property="og:url" content="' . $this->url . '">' . PHP_EOL;
         $metaTagOutput .= '<meta property="og:type" content="website">' . PHP_EOL;
         $metaTagOutput .= '<meta property="og:title" content="' . $this->title . '">' . PHP_EOL;
+        $metaTagOutput .= '<meta property="og:description" content="' . $this->description . '">' . PHP_EOL;
+        $metaTagOutput .= '<meta property="og:image" content="' . $this->image . '" />' . PHP_EOL;
 
-        if (isset($this->description) && !empty($this->description)) {
-            $metaTagOutput .= '<meta property="og:description" content="' . $this->description . '">' . PHP_EOL;
-        }
-        if (isset($this->image) && !empty($this->image)) {
-            $metaTagOutput .= '<meta property="og:image" content="' . $this->image . '" />' . PHP_EOL;
-        }
-
-        $metaTagOutput .= '<meta property="twitter:domain" content="' . $this->getSiteURL() . '">' . PHP_EOL;
-        $metaTagOutput .= '<meta property="twitter:url" content="' . $this->url . '">' . PHP_EOL;
-        $metaTagOutput .= '<meta name="twitter:title" content="' . $this->title . '">' . PHP_EOL;
-
-        if (isset($this->description) && !empty($this->description)) {
-            $metaTagOutput .= '<meta name="twitter:description" content="' . $this->description . '">' . PHP_EOL;
-        }
         if (isset($this->image) && !empty($this->image)) {
             $metaTagOutput .= '<meta name="twitter:card" content="summary_large_image">' . PHP_EOL;
-            $metaTagOutput .= '<meta property="twitter:image" content="' . $this->image . '" />' . PHP_EOL;
         } else {
             $metaTagOutput .= '<meta name="twitter:card" content="summary">' . PHP_EOL;
         }
-        
+        $metaTagOutput .= '<meta property="twitter:domain" content="' . $this->getSiteURL() . '">' . PHP_EOL;
+        $metaTagOutput .= '<meta property="twitter:url" content="' . $this->url . '">' . PHP_EOL;
+        $metaTagOutput .= '<meta name="twitter:title" content="' . $this->title . '">' . PHP_EOL;
+        $metaTagOutput .= '<meta name="twitter:description" content="' . $this->description . '">' . PHP_EOL;
+        $metaTagOutput .= '<meta property="twitter:image" content="' . $this->image . '" />' . PHP_EOL;
 
         return $metaTagOutput;
     }
