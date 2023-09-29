8eec10f9632324d76d322cc729327d8e82f8efc1
Thomas Neumann <57959550+tommyneu@users.noreply.github.com>
2023-09-29 16:42:36 -0500

    Added webcasts to ICS file download (#201)
    * Added webcasts to ICS file download
    
    * Fix stank and bugs

--- www/templates/default/icalendar/EventInstance.tpl.php
+++ www/templates/default/icalendar/EventInstance.tpl.php
@@ -11,7 +11,7 @@ if (isset($context->eventdatetime->starttime)) {
     if (strpos($context->eventdatetime->starttime,'00:00:00')) {
         $out[] = 'DTSTART;VALUE=DATE:' . $timezoneDateTime->format($context->getStartTime(),'Ymd');
     } else {
-           $out[] = 'DTSTART:'. $timezoneDateTime->formatUTC($context->getStartTime(),'Ymd\THis\Z');
+        $out[] = 'DTSTART:'. $timezoneDateTime->formatUTC($context->getStartTime(),'Ymd\THis\Z');
     }
 }
 if ($context->eventdatetime->recurringtype == 'none') {
@@ -28,12 +28,29 @@ $out[] = 'ORGANIZER;' . $organizer;
 $out[] = 'SUMMARY:' . icalFormatString($context->event->displayTitle($context));
 $out[] = 'STATUS:' . icalFormatString($context->event->icalStatus($context));
 $out[] = 'DESCRIPTION:' . icalFormatString($context->event->description);
-$l = $context->eventdatetime->getLocation();
-if (isset($context->eventdatetime->location_id) && $l !== false) {
-    $loc =  'LOCATION:'.$l->name;
+$location = $context->eventdatetime->getLocation();
+$webcast = $context->eventdatetime->getWebcast();
+$has_location = isset($context->eventdatetime->location_id) && $location !== false;
+$has_webcast = isset($context->eventdatetime->webcast_id) && $webcast !== false;
+if ($has_location && $has_webcast) {
+    $loc = 'LOCATION:' . $location->name;
     if (isset($context->eventdatetime->room)) {
-        $loc .=  ' Room '.$context->eventdatetime->room;
+        $loc .= ' Room ' . $context->eventdatetime->room;
+    } elseif (isset($location->room)) {
+        $loc .= ' Room ' . $location->room;
     }
+    $loc .= ', and online at ' . $webcast->title . ' (' . $webcast->url . ')';
+    $out[] = $loc;
+} elseif ($has_location) {
+    $loc = 'LOCATION:' . $location->name;
+    if (isset($context->eventdatetime->room)) {
+        $loc .= ' Room ' . $context->eventdatetime->room;
+    } elseif (isset($location->room)) {
+        $loc .= ' Room ' . $location->room;
+    }
+    $out[] = $loc;
+} elseif ($has_webcast) {
+    $loc = 'LOCATION:' . $webcast->title . '( ' . $webcast->url . ' )';
     $out[] = $loc;
 }
 $out[] = 'URL:'.$context->getURL();
@@ -42,7 +59,7 @@ if (isset($context->eventdatetime->endtime)
     if (strpos($context->eventdatetime->endtime,'00:00:00')) {
         $out[] = 'DTEND;VALUE=DATE:'. $timezoneDateTime->format($context->getEndTime(),'Ymd');
     } else {
-           $out[] = 'DTEND:' . $timezoneDateTime->formatUTC($context->getEndTime(),'Ymd\THis\Z');
+        $out[] = 'DTEND:' . $timezoneDateTime->formatUTC($context->getEndTime(),'Ymd\THis\Z');
     }
 } elseif (isset($context->eventdatetime->starttime)) {
     if (strpos($context->eventdatetime->starttime,'00:00:00')) {
