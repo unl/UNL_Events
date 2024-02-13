e5f94cf15c1df9fb03c38622aa8960dbae801df5
Thomas Neumann <57959550+tommyneu@users.noreply.github.com>
2024-02-13 14:51:39 -0600

    Remove dcf-uppercase from featured events (#211)

--- www/templates/default/html/EventListingSummary.tpl.php
+++ www/templates/default/html/EventListingSummary.tpl.php
@@ -40,17 +40,17 @@ use UNL\UCBCN\Event\Occurrence;
         $time = '';
         $time_icon = '<svg class="dcf-mr-1 dcf-h-4 dcf-w-4 dcf-flex-shrink-0 dcf-fill-current" aria-hidden="true" focusable="false" height="24" width="24" viewBox="0 0 24 24"><path d="M12 23C5.9 23 1 18.1 1 12S5.9 1 12 1s11 4.9 11 11-4.9 11-11 11zm0-20c-5 0-9 4-9 9s4 9 9 9 9-4 9-9-4-9-9-9z"></path><path d="M16.8 17.8c-.2 0-.5-.1-.7-.3l-5.2-4.8c-.2-.2-.3-.5-.3-.7V7.2c0-.6.4-1 1-1s1 .4 1 1v4.3l4.9 4.5c.4.4.4 1 .1 1.4-.3.3-.5.4-.8.4z"></path><path fill="none" d="M0 0h24v24H0z"></path></svg>';
         if ($eventinstance->isAllDay()) {
-            $time = '<div class="unl-event-time dcf-d-flex dcf-ai-center dcf-uppercase">' .
+            $time = '<div class="unl-event-time dcf-d-flex dcf-ai-center">' .
                 $time_icon .
                 'All day</div>';
         } else if ($eventinstance->eventdatetime->timemode === Occurrence::TIME_MODE_TBD) {
-            $time = '<div class="unl-event-time dcf-d-flex dcf-ai-center dcf-uppercase">' . $time_icon . '<abbr title="To Be Determined">TBD</abbr></div>';
+            $time = '<div class="unl-event-time dcf-d-flex dcf-ai-center">' . $time_icon . '<abbr title="To Be Determined">TBD</abbr></div>';
         } else if ($eventinstance->eventdatetime->timemode === Occurrence::TIME_MODE_START_TIME_ONLY) {
-            $time = '<time class="unl-event-time dcf-d-flex dcf-ai-center dcf-uppercase" datetime="' . $timezoneDisplay->format($starttime, $eventinstance->eventdatetime->timezone, 'H:i') . '">' . $time_icon . 'Starts at ' . $timezoneDisplay->format($starttime, $eventinstance->eventdatetime->timezone, 'g:i a') . '</time>';
+            $time = '<time class="unl-event-time dcf-d-flex dcf-ai-center" datetime="' . $timezoneDisplay->format($starttime, $eventinstance->eventdatetime->timezone, 'H:i') . '">' . $time_icon . 'Starts at ' . $timezoneDisplay->format($starttime, $eventinstance->eventdatetime->timezone, 'g:i a') . '</time>';
         } else if ($eventinstance->eventdatetime->timemode === Occurrence::TIME_MODE_END_TIME_ONLY) {
-            $time = '<time class="unl-event-time dcf-d-flex dcf-ai-center dcf-uppercase" datetime="' . $timezoneDisplay->format($endtime, $eventinstance->eventdatetime->timezone, 'H:i') . '">' . $time_icon . 'Ends at ' . $timezoneDisplay->format($endtime, $eventinstance->eventdatetime->timezone, 'g:i a') . '</time>';
+            $time = '<time class="unl-event-time dcf-d-flex dcf-ai-center" datetime="' . $timezoneDisplay->format($endtime, $eventinstance->eventdatetime->timezone, 'H:i') . '">' . $time_icon . 'Ends at ' . $timezoneDisplay->format($endtime, $eventinstance->eventdatetime->timezone, 'g:i a') . '</time>';
         } else {
-            $time = '<div class="unl-event-time dcf-d-flex dcf-ai-center dcf-uppercase">' .
+            $time = '<div class="unl-event-time dcf-d-flex dcf-ai-center">' .
                 $time_icon .
                 '<time datetime="' .
                 $timezoneDisplay->format($starttime, $eventinstance->eventdatetime->timezone, 'H:i') .
